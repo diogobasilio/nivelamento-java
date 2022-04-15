c8d2a3b3b53f657dae192de9cc206fa6941f021f
Diogo Basílio <77036678+diogobasilio@users.noreply.github.com>
2022-04-15 15:09:45 -0300

    Primeira leva de exercicios

--- 001.java
+++ 001.java
@@ -1,3 +1,8 @@
+/**Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+   mensagem explicativa, conforme exemplos.*/
+
+
+
 import java.util.Scanner;
 
 public class Main {
