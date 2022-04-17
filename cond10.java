2154a0b753cd4f2d5fa226847fb34c560172f5c8
Diogo Basílio <77036678+diogobasilio@users.noreply.github.com>
2022-04-16 22:13:42 -0300

    expressão condicional ternária

--- cond10.java
+++ cond10.java
@@ -10,7 +10,7 @@ public class Main {
 		double preco = 34.5;
 		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
 		
-		System.out.printf("%.2f\n"desconto);
+		System.out.printf("%.2f\n",desconto);
 		
 	}
 }
