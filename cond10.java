bb8fd9a245b7d177ec0c49717a7870b2eaed01d9
Diogo Basílio <77036678+diogobasilio@users.noreply.github.com>
2022-04-16 22:12:33 -0300

    expressão condicional ternária

---
+++ cond10.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		
+		System.out.printf("%.2f\n"desconto);
+		
+	}
+}
