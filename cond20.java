fce394a312d02055c642eb18d78fb7e5721dd951
Diogo Basílio <77036678+diogobasilio@users.noreply.github.com>
2022-04-21 16:17:42 -0300

    Operadores bitwise

---
+++ cond20.java
@@ -0,0 +1,27 @@
+/**  Os operadores bitwise são aqueles operadores que realizam operações lógicas bit a bit em valores.
+ *   sendo mais utilizados em baixo nível, como arduíno, micro controleador, programação de interface com rede.
+ */
+
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int mask = 0b100000;
+		int n = sc.nextInt();
+		
+		if ((n & mask) != 0 ) {
+			System.out.printf("O 6° bit de %d é true", n);
+		} else {
+			System.out.printf("O 6° bit de %d é false", n);
+		}
+		
+		sc.close();
+	}
+}
